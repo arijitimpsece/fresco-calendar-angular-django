5f7c5ada0c94b7a84de00867c8ca5f1c33828116
arijitimpsece <167027501+arijitimpsece@users.noreply.github.com>
2024-04-15 00:47:07 +0530

    Angular part added

---
+++ angular/src/app/authentication/authentication.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HolidayService } from '../services/holiday.service';
+import { FormGroup, Validators, FormControl, FormBuilder } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+@Component({
+  selector: 'app-authentication',
+  templateUrl: './authentication.component.html',
+  styleUrls: ['./authentication.component.css']
+})
+export class AuthenticationComponent implements OnInit {
+  hide = false;
+  // loginForm: FormGroup;
+  loginError: string = '';
+  /**
+   * signInForm should contain the following Form control
+   * 1. userName -> validate email, required.
+   * 2. password -> required
+   */
+
+  signInForm: FormGroup;
+
+  constructor(private holidayServiceObj: HolidayService,  private fb: FormBuilder,
+   private snackBar: MatSnackBar, private route: Router, ) { 
+  this.signInForm = this.fb.group({
+      userName: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });}
+
+  get userName() {
+    return this.signInForm.get('userName');
+  }
+
+  get password() {
+    return this.signInForm.get('password');
+  }
+  ngOnInit() : void{
+  }
+
+  /**
+   * On click signIn button should call validate()
+   * The validate should use the signIn function in HolidayService
+   * If response is {status:1} then navigate to dashboard
+   * Otherwise display "Invalid password" using snackbar
+   */
+ 
+
+  validate(): void {
+    if (this.signInForm.invalid) {
+      this.snackBar.open('Invalid password', 'Close');
+      return;
+    }
+    
+    this.holidayServiceObj.signIn(this.userName.value, this.password.value).subscribe(
+      response => {
+        if (response.status == 1) {
+           this.route.navigate(['/dashboard']);
+         }  else {
+          console.error('Login failed:', error);
+          this.snackBar.open('Invalid password');
+         }
+        // Handle successful login response here
+      },
+      error => {
+        console.error('Login failed:', error);
+        this.snackBar.open('Invalid password');
+        // this.loginError = 'Invalid email or password';
+        // Handle login error here
+      }
+    );
+  }
+
+}
